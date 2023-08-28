7fdb96d22529065495e3b8680e00831fa6733776
watagassa <k23095kk@aitech.ac.jp>
2023-08-29 00:47:54 +0900

    “reactTest”

---
+++ my-app/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+import './App.css';
+import { ColoredMessage } from './components/ColoredMessage';
+function App() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>gitのテスト用</h1>
+      <h1 style = {{color: "red"}}>こんにちは！</h1>
+            <ColoredMessage color = "blue">hasimoto</ColoredMessage>
+            <ColoredMessage color = "pink"></ColoredMessage>
+    </>
+  )
+}
+
+export default App
